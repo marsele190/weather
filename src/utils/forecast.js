c44aaf0178254f7f6459cee2d45830a0f6a9be94
Kostiuk <dmytro.kostiuk@vaillant-group.com>
2019-07-13 15:43:00 +0200

    Add space

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,8 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            console.log(body.daily.data[0])
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out with a low of '+body.daily.data[0].temperatureLow+'. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out with a low of '+body.daily.data[0].temperatureLow+' and high of '+body.daily.data[0].temperatureHigh+'. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
